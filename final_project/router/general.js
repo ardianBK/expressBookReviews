eff022d8a0141f56889a8f77352b32ff11ace1a9
“ardianbk96” <ardianbk96@gmail.com>
2024-04-22 23:02:52 -0400

    task 1-5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,52 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: books});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let number = req.params.isbn
+  let result = books[number]
+  return res.status(300).json({message: result});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let authorData
+    for (let key in books) {
+        if (books.hasOwnProperty(key)) {
+             if (books[key].author === req.params.author) {
+                authorData = books[key]
+            }
+        }
+    }
+
+  return res.status(300).json({message: authorData});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let titleData
+    for (let key in books) {
+        if (books.hasOwnProperty(key)) {
+             if (books[key].title === req.params.title) {
+                titleData = books[key]
+            }
+        }
+    }
+  return res.status(300).json({message: titleData});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let number = req.params.isbn
+  let result = books[number]
+  return res.status(300).json({message: result.reviews});
 });
 
 module.exports.general = public_users;
